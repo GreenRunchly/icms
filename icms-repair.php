f916abc925c990a20f6baef8c67921bb0f97df7f
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-12-27 02:01:44 +0700

    Update icms-repair.php

--- icms-repair.php
+++ icms-repair.php
@@ -7,24 +7,27 @@
 
 		if ( $updatestatus[0][0] != $updatestatus[0][1] ){
 
-			$updatefile = file_put_contents('update.zip',file_get_contents('https://github.com/GreenRunchly/icms/archive/refs/heads/main.zip'));
+			$randomea = md5( floor( microtime(true) * 1000 ) );
+
+			$updatefile = file_put_contents( $randomea . '.zip',file_get_contents('https://github.com/GreenRunchly/icms/archive/refs/heads/main.zip'));
 
 			$zip = new ZipArchive;
-			$openedzipfile = $zip->open('update.zip');
+			$openedzipfile = $zip->open( $randomea . '.zip');
 			if ($openedzipfile == true) {
 				
 				foreach ( $updatestatus[1] as $zipfilekey => $zipfilename) {
 					$datafile = $zip->getFromName( $zipfilename );
 					
-					if ( ! file_exists( ltrim( str_ireplace( "icms-main", '', $zipfilename), '/' ) ) ) {
-						mkdir( dirname( ltrim( str_ireplace( "icms-main", '', $zipfilename), '/' ) ) );
+					if ( ! is_dir( ltrim( str_ireplace( "icms-main", '', $zipfilename), '/' ) ) ) {
+						if ( ! file_exists( ltrim( str_ireplace( "icms-main", '', $zipfilename), '/' ) ))
+						mkdir( dirname( ltrim( str_ireplace( "icms-main", '', $zipfilename), '/' ) ) , 0777, true);
 					}
 					file_put_contents( ltrim( str_ireplace( "icms-main", '', $zipfilename), '/' ) , $datafile);
 				}
 
 				$zip->close();
 
-				//unlink('update.zip');
+				unlink( $randomea . '.zip');
 			}
 
 		}else{
