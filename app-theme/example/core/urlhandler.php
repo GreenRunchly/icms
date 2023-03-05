40b5cd69b65f848ac62743c1139a495a65ac8223
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2023-03-05 12:31:50 +0700

    Update urlhandler.php

--- app-theme/example/core/urlhandler.php
+++ app-theme/example/core/urlhandler.php
@@ -1,3 +1,3 @@
 <?php 
-	if ( file_exists( app_theme_dir().'/404.php' ) ) { include app_theme_dir().'/404.php'; } else { echo "404 - create '404.php' file in your theme to make your own error page."; }; /// This will be displayed if there's no other pages 
+	app_theme_404();
 ?>
